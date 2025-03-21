1a8d03860cdc52dd8d22499e5f042b6c26a39516
Guillaume BITON <guillaume@gbweb.fr>
2025-03-21 15:15:38 +0100

    Adding missing POST methods to routes + tagging latest-dev to CI/CD

--- src/zigbee2mqtt_maas_power/routes.py
+++ src/zigbee2mqtt_maas_power/routes.py
@@ -11,7 +11,7 @@ def register_routes(app):
         """
         return jsonify({"nodes": list(app.config["nodes"].keys())})
 
-    @app.route('/nodes/<node_name>/power-on', methods=["GET"])
+    @app.route('/nodes/<node_name>/power-on', methods=["GET", "POST"])
     def power_on_node(node_name):
         if node_name not in app.config["nodes"]:
             app.logger.warning("Node \"%s\" not found.", node_name)
@@ -20,7 +20,7 @@ def register_routes(app):
         node.power_on()
         return jsonify({"status": "ok"})
 
-    @app.route('/nodes/<node_name>/power-off', methods=["GET"])
+    @app.route('/nodes/<node_name>/power-off', methods=["GET", "POST"])
     def power_off_node(node_name):
         if node_name not in app.config["nodes"]:
             app.logger.warning("Node \"%s\" not found.", node_name)
@@ -29,7 +29,7 @@ def register_routes(app):
         node.power_off()
         return jsonify({"status": "ok"})
 
-    @app.route('/nodes/<node_name>/state', methods=["GET"])
+    @app.route('/nodes/<node_name>/state', methods=["GET", "POST"])
     def node_state(node_name):
         if node_name not in app.config["nodes"]:
             app.logger.warning("Node \"%s\" not found.", node_name)
